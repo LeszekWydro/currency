4fa44e8b6701384a9ecd0c5cd49761e852d557b4
Leszek Wydro <leszek.wydro@gmail.com>
2022-05-03 14:07:04 +0200

    Add tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -2,8 +2,16 @@ export const convertPLNToUSD = (PLN) => {
   const PLNtoUSD = PLN / 3.5;
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
 
+  if (typeof (PLN) === 'string' || (PLN) === undefined) {
+    return NaN;
+  } else if (typeof (PLN) !== 'string' && typeof (PLN) !== 'number') {
+    return 'Error';
+  } else if ((PLN) <= 0) {
+    return '$0.00';
+  }
+
   return formatter.format(PLNtoUSD);
-}
+};
